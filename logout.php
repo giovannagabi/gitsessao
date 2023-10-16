dc8d8b37c79130f2b2ad49da3281a25cb8fe02f7
giovannagabi <giovannagabrielly2302@gmail.com>
2023-10-16 13:43:30 -0300

    Logout

---
+++ logout.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="p-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+</head>
+<body>
+    <h1>Logout</h1>
+</body>
+</html>
